e553b373bee37b12b7229bf82c17ff65d85b61db
Bruno Magalhães Morgado <bruno.morgado@wooza.com.br>
2019-09-30 14:09:56 -0300

    feat:TASK - Adicionando startup

---
+++ Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using ProductCatalog.Data;
+using ProductCatalog.Repositories;
+
+namespace ProductCatalog
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddScoped<StoreDataContext,StoreDataContext>();
+            services.AddTransient<ProductRepository,ProductRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
